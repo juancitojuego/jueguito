56385dc3a672c20c3a321108007e71f5a648a923
juancito <juancitojuego7@gmail.com>
2025-06-04 05:06:25 +0100

    start again

--- src/interfaces/index.ts
+++
@@ -1,7 +0,0 @@
-// src/interfaces/index.ts
-
-export * from './stone';
-export * from './gameState';
-export * from './gameStateManager';
-export * from './randomService';
-export * from './fightService';
